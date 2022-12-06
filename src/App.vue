674f65834fba6af1a4045e671056d0fc6a71e2ff
Gemma Batlle <gembb15@gmail.com>
2022-12-06 15:45:09 +0100

    finishing the lab

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,43 @@
 <script>
-
+import { computed } from "@vue/reactivity";
+import ITEMS from "./items.json";
+  export default {
+    data() {
+      return {
+        numberA: 5,
+        numberB: 16,
+        items: ITEMS,
+        message: "This is a normal message",
+      }
+    },
+    computed: {
+      itemName(){
+        return this.items[0].name;
+      },
+      cartLimit(){
+        return this.items.length >= 5;
+      },
+    }
+  }
 </script>
 
 <template>
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <p class="text-right m-6">{{(numberA + numberB)}} April, 2022</p>
+  <p class="text-3xl m-6">The {{itemName}} Project</p>
+  <div class="border-2 border-blue-500">
+    <div v-for="item of items" key="item.uuid" class="flex justify-evenly my-8">
+      <h3 class="text-xl font-bold">{{item.name}}</h3>
+      <div class="flex flex-col items-center">
+        <img :src="item.image" :alt="item.name" class="w-16">
+        <p>{{item.description}}</p>
+      </div>
+      <p>{{item.price}}</p>
+    </div>
+  </div>
+  <div>
+    <button v-if="cartLimit" class="float-right m-12 border-2 border-blue-500 p-2">Confirm purchase</button>
+    <p v-else>You need to add more products</p>
+  </div>
 </template>
 
 <style scoped>
